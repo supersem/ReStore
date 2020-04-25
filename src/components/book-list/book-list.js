b53d8ca5f916fc4c06d44fe5be5d6b1be994b88b
supersem <xsupersemx@gmail.com>
2020-04-25 17:08:03 +0300

    add some styles and asynchronous data

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -7,19 +7,25 @@ import { booksLoaded } from '../../actions';
 import { compose } from '../../utils';
 import './book-list.css';
 
+import Spinner from '../spinner';
+
 class BookList extends Component {
 
   componentDidMount() {
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-
-    this.props.booksLoaded(data);
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks()
+      .then((data) => booksLoaded(data));
   }
 
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
+
     return (
-      <ul>
+      <ul className="book-list" >
         {
           books.map((book) => {
             return (
@@ -32,8 +38,8 @@ class BookList extends Component {
   };
 };
 
-const mapStateToProps = ({ books }) => {
-  return { books };
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading };
 };
 
 const mapDispatchToProps = {
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,12 +1,21 @@
 export default class BookstoreService {
+  data = [
+    { id: 1,
+      title: 'Production-Ready Micro',
+      author: 'Susan Fowler',
+      price: 32,
+      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'},
+    { id: 2,
+      title: 'React+Redux',
+      author: 'Juriy Bura',
+      price: 45,
+      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'}
+  ];
   getBooks() {
-    return [
-      { id: 1,
-        title: 'Production-Ready Micro',
-        author: 'Susan Fowler'},
-      { id: 2,
-        title: 'React+Redux',
-        author: 'Juriy Bura'}
-    ];
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.data)
+      }, 700);
+    });
   }
 }
