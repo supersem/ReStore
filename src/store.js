8490d4046e3553dae38203bc7a1cb2541ae6b37d
supersem <xsupersemx@gmail.com>
2020-05-01 14:08:21 +0300

    middleware

--- src/store.js
+++ src/store.js
@@ -1,38 +1,25 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const logEnchancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-  const originalDispatch = store.dispatch;
-  store.dispatch = (action) => {
-    console.log(action.type);
-    return originalDispatch(action);
-  };
 
-  return store;
+const logMiddleware = ({ getState }) => (next) => (action) => {
+  console.log(action.type, getState());
+  return next(action);
 };
 
-const stringEnchancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-  const originalDispatch = store.dispatch;
-  store.dispatch = (action) => {
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action
+    });
+  }
 
-    if (typeof action === 'string') {
-      return originalDispatch({
-        type: action
-      });
-    }
-
-    return originalDispatch(action);
-  };
-
-  return store;
+  return next(action);
 };
 
-const store = createStore(reducer, compose(
-  stringEnchancer,
-  logEnchancer));
+const store = createStore(reducer, applyMiddleware(
+  stringMiddleware, logMiddleware));
 
 store.dispatch('HELLO_WORLD');
 
